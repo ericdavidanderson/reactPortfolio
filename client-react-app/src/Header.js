32dabc3aa620cefdddc36dbfafa9f5a4ab0bf740
ericdavidanderson <burrej1@gmail.com>
2022-01-10 18:45:44 -0800

    navigation and header linked to app.js

---
+++ client-react-app/src/Header.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import Navigation from './Navigation';
+
+export default function Header() {
+    return (
+      <header>
+        <h1>Welcome to Eric Anderson's page</h1>
+        <Navigation/>
+      </header>
+    );
+}
---
+++ client-react-app/src/Navigation.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Navigation() {
+    return (
+     
+        <div class="Links">
+          <ul class="ul">
+            <li>
+              {" "}
+              <a href="https://github.com/ericdavidanderson?tab=repositories">
+                {" "}
+                Portfolio
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a href="https://linkedin.com/in/eric-anderson-43bb05106">
+                Linkedin
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a href="tel:+14252190401"> Contact</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="./assets/about me.md"> About me </a>{" "}
+            </li>
+          </ul>
+        </div>
+      
+    );
+}
